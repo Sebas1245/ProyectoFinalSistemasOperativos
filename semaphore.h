d63ae56730ea52eb467b6f4f8aadadcb6bc25d46
Lorena Ang <45380244+lorena-ang@users.noreply.github.com>
2021-02-02 00:36:40 -0600

    Problema 7
    Productor - Consumidor

---
+++ semaphore.h
@@ -0,0 +1,37 @@
+#include <mutex>
+#include <condition_variable>
+
+class Semaphore
+{
+public:
+  Semaphore(int count_ = 0)
+      : count(count_)
+  {
+  }
+
+  //Funcion para aumentar semaforo/liberar un recurso
+  inline void V()
+  {
+    std::unique_lock<std::mutex> lock(mtx);
+    count++;
+    //Avisa que se libero +1 en el semaforo
+    cv.notify_one();
+  }
+
+  //Funcion para checar si hay disponibilidad y bloquear si no
+  inline void P()
+  {
+    std::unique_lock<std::mutex> lock(mtx);
+    while (count == 0)
+    {
+      //Esperar hasta que haya una V/el semaforo este en 1 o mas
+      cv.wait(lock);
+    }
+    count--; //Resta un recurso
+  }
+
+private:
+  std::mutex mtx;
+  std::condition_variable cv;
+  int count;
+};
