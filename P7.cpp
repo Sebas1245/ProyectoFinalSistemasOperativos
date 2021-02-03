21bf1049a4da2c2c6198ffcbcd4704ed4c8c3e7c
Sebastián Saldaña <A01570274@itesm.mx>
2021-02-02 18:56:58 -0600

    Cambios finales a P7

--- P7.cpp
+++ P7.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 #include "semaphore.h"
 
 using namespace std;
 
 Semaphore sDisponible(2); // Semaforo que controla la cantidad de semaforos disponibles
-Semaphore sBuffer(0); // Semaforo que indica si hay espacios ocupados
-int coop = 0; // Contador de cooperaciones exitosas - cuando el consumidor consume lo producido
+Semaphore sBuffer(0);     // Semaforo que indica si hay espacios ocupados
+int coop = 0;             // Contador de cooperaciones exitosas - cuando el consumidor consume lo producido
 
 void Productor()
 {
@@ -30,6 +31,43 @@ void Consumidor()
 
 int RunP7()
 {
+  vector<thread> tP;
+  vector<thread> tC;
+  int iP, iC;
+
+  cout << "¿Cuántos productores?: " << endl;
+  cin >> iP;
+  cout << "¿Cuántos consumidores?: " << endl;
+  cin >> iC;
+
+  // Creación de procesos de productor
+  for (int i = 0; i < iP; i++)
+  {
+    tP.emplace_back(Productor);
+  }
+
+  // Creación de procesos de consumidor
+  for (int i = 0; i < iC; i++)
+  {
+    tC.emplace_back(Consumidor);
+  }
+
+  // Se termina la ejecucion de los threads y se unen al main
+  for (auto &t : tP)
+  {
+    t.join();
+  }
+
+  for (auto &t : tC)
+  {
+    t.join();
+  }
+
+  cout << "\nCooperaciones exitosas: " << coop << endl;
+
+  /* 
+  // Input manual de productores y consumidores
+
   thread t1(Productor);
   thread t2(Consumidor);
   thread t3(Productor);
@@ -43,8 +81,7 @@ int RunP7()
   t4.join();
   t5.join();
   t6.join();
-
-  cout << "\nCooperaciones exitosas: " << coop << endl;
+  */
 
   return 0;
 }
